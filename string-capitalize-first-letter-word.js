b0efdd5b48263063176c35669e310813f4383213
Sachin Chanduveettil <sachisavera@gmail.com>
2023-03-30 22:17:43 +0530

    new code challenge added

---
+++ string-capitalize-first-letter-word.js
@@ -0,0 +1,11 @@
+const capitalized = function (sentance){
+    let str = sentance.split(" ");
+    for(let i = 0; i < str.length; i++){
+        console.log(str[i][0].toUpperCase() + str[i].substring(1));
+    }
+
+    return str.join(" ");
+}
+
+console.log(capitalized("i love india"));
+console.log(capitalized("hardwork is not easy"));
