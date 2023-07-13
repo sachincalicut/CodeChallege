d05a7f0914d07a3b6591ff524566aa52d5c0fd55
Sachin Chanduveetil <sachisavera@gmail.com>
2023-07-13 15:09:17 +0530

    Closure.js
    Create Closure function in Javascript with sample code

---
+++ Closure.js
@@ -0,0 +1,22 @@
+// javascript closure example
+
+// outer function
+function greet() {
+
+    // variable defined outside the inner function
+    let name = 'John';
+
+    // inner function
+    function displayName() {
+
+        // accessing name variable
+        return 'Hi' + ' ' + name;
+      
+    }
+
+    return displayName;
+}
+
+const g1 = greet();
+console.log(g1); // returns the function definition
+console.log(g1()); // returns the value
