1ac242119be815ef82b96ba55951eb8d9b247938
Sachin Chanduveetil <sachisavera@gmail.com>
2023-10-24 14:26:09 +0530

    Update remove-duplicates-array-value.js

--- remove-duplicates-array-value.js
+++ remove-duplicates-array-value.js
@@ -1,10 +1,57 @@
 // Remove duplicate array value in javascript?
  let arr = ["scale", "happy", "strength",
             "peace", "happy", "happy"];
- 
-    function removeDuplicates(arr) {
+// Example 1 - Using Filter
+  
+function removeDuplicates(arr) {
         return arr.filter((item,
             index) => arr.indexOf(item) === index);
     }
  
     console.log(removeDuplicates(arr));
+// Example 2 - Using  FoEach
+ 
+    function removeDuplicates(arr) {
+        let unique = [];
+        arr.forEach(element => {
+            if (!unique.includes(element)) {
+                unique.push(element);
+            }
+        });
+        return unique;
+    }
+ 
+    console.log(removeDuplicates(arr));
+// Example 3 - Using Reduce
+
+   function removeDuplicates(arr) {
+        let unique = arr.reduce(function (acc, curr) {
+            if (!acc.includes(curr))
+                acc.push(curr);
+            return acc;
+        }, []);
+        return unique;
+    }
+ 
+    console.log(removeDuplicates(arr));
+
+// Example 4 - IndexOf
+   function removeDuplicates(arr) {
+        let unique = [];
+        for(i=0; i < arr.length; i++){ 
+            if(unique.indexOf(arr[i]) === -1) { 
+                unique.push(arr[i]); 
+            } 
+        }
+        return unique;
+    }
+ 
+    console.log(removeDuplicates(arr));
+// Example 5 - setOf
+
+function removeDuplicates(arr) {
+        return arr.filter((item,
+            index) => arr.indexOf(item) === index);
+    }
+ 
+  console.log(removeDuplicates(arr));
