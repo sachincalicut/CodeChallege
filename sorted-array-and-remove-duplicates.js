e251c6ea78c1adfcef74cb73e1c8b4076b3af084
Sachin Chanduveetil <sachisavera@gmail.com>
2023-11-09 08:45:57 +0530

    Update sorted-array-and-remove-duplicates.js

--- sorted-array-and-remove-duplicates.js
+++ sorted-array-and-remove-duplicates.js
@@ -1,4 +1,6 @@
 // Remove duplicates and sorted array in javascript ?
+//Output
+// [0, 1, 2, 3, 4, 5]
 function removeDuplicate(nums){
     let i=0;
     for(let j=1; j<nums.length; j++){
@@ -11,3 +13,4 @@ function removeDuplicate(nums){
     return nums;
 }
 console.log(removeDuplicate([0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 4, 4, 5]));
+
