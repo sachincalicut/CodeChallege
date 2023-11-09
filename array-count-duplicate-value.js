e4450f45027eb951fa10096ab520ed5edecba7f0
Sachin Chanduveetil <sachisavera@gmail.com>
2023-11-09 09:14:50 +0530

    Update array-count-duplicate-value.js

--- array-count-duplicate-value.js
+++ array-count-duplicate-value.js
@@ -11,3 +11,20 @@ var map = arr.reduce(function(prev, cur) {
 
 // map is an associative array mapping the elements to their frequency:
 console.log(map);
+
+// Output
+// prints {1:2, 2:3, 3:1, 4:1, 5:1}
+
+const arr = [1,2,2,2,4,3,5,1];
+
+const res = arr.reduce((x, y) => {
+	if(x[y]){
+  		x[y] += 1;
+  }
+  else{
+  	x[y] = 1;
+  }
+  return x;
+}, {})
+
+console.log(res);
