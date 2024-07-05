7c03f1a7ed3d90e945d462d51a7f2fdec9a79165
Sachin Chanduveetil <sachisavera@gmail.com>
2024-07-05 09:13:31 +0530

    Update JavaScriptQ&A1.0.js

--- JavaScriptQ&A1.0.js
+++ JavaScriptQ&A1.0.js
@@ -47,5 +47,23 @@ console.log("5");
 "5"
 "4"
 "2"
+// Javascript Flattern Numbers find
+const numbers = [
+	1,
+	[3, [2, 8, [12]], 9],
+	[5],
+	[12, [[5]]],
+	[100, [23, 45]]
+];
+const flatternNumb = (numbers) => {
+	return numbers.reduce((acc, num) => {
+		return acc.concat(Array.isArray(num) ? 
+		flatternNumb(num) : num);
+	}, []);
+};
+console.log(flatternNumb(numbers));
+// using inbuild function
+console.log(numbers.flat(Infinity));
 
-
+//output
+//[1,3,2,8,12,9,5,12,5,100,23,45]
